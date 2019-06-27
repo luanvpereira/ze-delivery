19e5d89f105b2862eeff0656790b1571621c9eec
Luan Pereira <pereiralv2010@gmail.com>
2019-06-27 03:06:47 -0300

    alterado configurações do eslint

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    verbose: true,
-    moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$':
-            '<rootDir>/__mocks__/file-mock.js',
-        '\\.(s?css)$': 'identity-obj-proxy'
-    },
-    setupFiles: ['./test-utils/enzyme-setup.js'],
-    coveragePathIgnorePatterns: ['types', 'test-utils', 'pages', 'constants'],
-    collectCoverage: true,
-    coverageThreshold: {
-        global: {
-            branches: 100,
-            functions: 100,
-            lines: 100,
-            statements: 100
-        }
-    }
+	verbose: true,
+	moduleNameMapper: {
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$':
+			'<rootDir>/__mocks__/file-mock.js',
+		'\\.(s?css)$': 'identity-obj-proxy'
+	},
+	setupFiles: ['./test-utils/enzyme-setup.js'],
+	coveragePathIgnorePatterns: ['src/types', 'test-utils', 'src/pages', 'src/constants'],
+	collectCoverage: true,
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100
+		}
+	}
 };
